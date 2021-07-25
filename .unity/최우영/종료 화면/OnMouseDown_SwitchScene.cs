8b4b89952960520fb4ed78aad76a47dd38c3098e
wyoung163 <80168342+wyoung163@users.noreply.github.com>
2021-07-25 23:59:47 +0900

    Add files via upload

---
+++ .unity/최우영/종료 화면/OnMouseDown_SwitchScene.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnMouseDown_SwitchScene : MonoBehaviour
+{
+    public string sceneName;
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(sceneName);   
+    }
+}
